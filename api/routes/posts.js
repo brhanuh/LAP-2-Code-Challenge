b392aa8a83391a0c1b4b4815b84304a4288b7973
brhanuh <hanibalbrhanu15@hotmail.com>
2022-06-22 17:51:48 +0100

    implement view all posts for client

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -3,7 +3,5 @@ const router = express.Router();
 const postsController = require('../controllers/posts')
 
 router.get('/', postsController.index)
-router.get('/:id', postsController.show)
-router.post('/', postsController.create)
 
 module.exports = router;
